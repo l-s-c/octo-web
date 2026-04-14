32e8c4fe07621991bd69ddb9cc95f94edac5275b
quanming1 <quanming1-bf33e77a6f7f@users.noreply.github.com>
2026-04-14 17:52:12 +0800

    feat(VirtualTable): add exports

---
+++ packages/dmworkbase/src/Components/VirtualTable/index.ts
@@ -0,0 +1,4 @@
+export { VirtualTable } from "./VirtualTable";
+export type { VirtualTableProps, ColumnConfig } from "./VirtualTable";
+export { useVirtualScroll, calculateVirtualScroll } from "./useVirtualScroll";
+export type { UseVirtualScrollOptions, VirtualScrollResult } from "./useVirtualScroll";
